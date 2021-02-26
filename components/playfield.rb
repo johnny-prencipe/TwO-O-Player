800f99f293eaef588126048a93c7fc4d9a298bce
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-02-26 09:29:14 -0800

    Write game logic;

--- components/playfield.rb
+++ components/playfield.rb
@@ -1,15 +1,38 @@
-class Playfield
-  attr_accessor :Playfield
-
-  def initialize(player_one, player_two)
-    @player_one = player_one
-    @player_two = player_two
-
-    @first_add  = rand(1...20)
-    @second_add = rand(1...20)
-    @sum        = @first_add + @second_add
+class Game
+  attr_accessor :game
+  def initialize(p1, p2)
+    @p1 = p1
+    @p2 = p2
   end
-
-  puts "#{@player_one}, what is the sum of #{@first_add} and #{@second_add}?"
-
+  def game
+    game_master = Question.new
+    turn = 0
+    @player = nil
+    while @p1.lives > 0 || @p2.lives > 0
+      if turn % 2 == 0 
+        @player = @p1
+        turn += 1
+      else
+        @player = @p2
+        turn += 1
+      end
+      game_master.current_player(@player)
+      if @p2.lives == 0
+        return puts "#{@player.name} Wins!"
+      elsif @p1.lives == 0
+        return puts "#{@player.name} Wins!"
+      end
+      game_master.ask_question()
+      print "#{@player.name}> "
+      answer = $stdin.gets.chomp.to_i
+      if game_master.check(answer)
+        puts "YES!! You are correct."
+      else
+        puts "Seriously? No!"
+        @player.lives -= 1
+      end
+      puts "Player 1: #{@p1.lives}/3 vs Player 2: #{@p2.lives}/3"
+      puts "----- NEW TURN -----"
+    end
+  end
 end
