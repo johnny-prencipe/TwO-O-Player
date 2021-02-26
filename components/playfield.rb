85f2025c4c7ccddf26dd9ccbd9b9578c7115e40d
Johnny Prencipe <johnny.prencipe@gmail.com>
2021-02-26 09:30:11 -0800

    Formatting

--- components/playfield.rb
+++ components/playfield.rb
@@ -1,13 +1,16 @@
 class Game
+
   attr_accessor :game
   def initialize(p1, p2)
     @p1 = p1
     @p2 = p2
   end
+
   def game
     game_master = Question.new
     turn = 0
     @player = nil
+
     while @p1.lives > 0 || @p2.lives > 0
       if turn % 2 == 0 
         @player = @p1
@@ -16,14 +19,18 @@ class Game
         @player = @p2
         turn += 1
       end
+
       game_master.current_player(@player)
+
       if @p2.lives == 0
         return puts "#{@player.name} Wins!"
       elsif @p1.lives == 0
         return puts "#{@player.name} Wins!"
       end
+
       game_master.ask_question()
       print "#{@player.name}> "
+
       answer = $stdin.gets.chomp.to_i
       if game_master.check(answer)
         puts "YES!! You are correct."
@@ -31,8 +38,10 @@ class Game
         puts "Seriously? No!"
         @player.lives -= 1
       end
+
       puts "Player 1: #{@p1.lives}/3 vs Player 2: #{@p2.lives}/3"
       puts "----- NEW TURN -----"
     end
+    
   end
 end
